2913cd3fa100ef0b13713fe6c689207cc3a9299f
Deepali Verma <deepaliverma064@gmail.com>
2024-11-22 20:39:40 +0530

    data base connection setup

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,4 +3,7 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./config/schema.js",
   out: "./drizzle",
+  dbCredentials: {
+    url:"postgresql://shotsdb_owner:1F6pBjqrHCgL@ep-lucky-voice-a120cpxy.ap-southeast-1.aws.neon.tech/Ai-short-videos?sslmode=require",
+  }
 });
