78b35d13670c5f49d2772123337f9e86776cb3b8
Deepali Verma <deepaliverma064@gmail.com>
2024-12-14 20:42:48 +0530

    adding backend

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -3,10 +3,7 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="grid grid-col-1 md:grid-cols-2 bg-slate-800">
-      <div >
-        <Image src={'/login1.png'} alt="login" width={500} height={800} className="w-full object-contain"/>
-      </div>
+    <div className=" bg-slate-800">
       <div
       className="flex justify-center items-center h-screen"><SignIn/></div>
     </div>
--- app/provider.js
+++ app/provider.js
@@ -1,11 +1,34 @@
-import React from 'react'
+"use client"
 
-const Provider = ({children}) => {
-  return (
-    <div>
-      {children}
-    </div>
-  )
-}
+import { db } from "@/config/db";
+import { Users } from "@/config/schema";
+import { useUser } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import { useEffect } from "react";
 
-export default Provider
+const Provider = ({ children }) => {
+  const {user} = useUser();
+
+  useEffect(()=>{
+  user&&isNewUser();
+  },[user])
+
+  const isNewUser = async () => {
+    const result = await db
+      .select()
+      .from(Users)
+      .where(eq(Users.email, user?.primaryEmailAddress?.emailAddress));
+
+      if(!result[0]){
+      db.insert(Users).values({
+          name: user.fullName,
+          email: user?.primaryEmailAddress?.emailAddress,
+          imageUrl:user?.imageUrl
+        })
+      }
+  };
+
+  return <div>{children}</div>;
+};
+
+export default Provider;
